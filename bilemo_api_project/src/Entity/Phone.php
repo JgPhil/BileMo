b400e28dee528c339f6e945a4d4296afca914367
JgPhil <jamingph@gmail.com>
2020-08-26 14:55:08 +0200

    Unique Entity Phone name field

--- bilemo_api_project/src/Entity/Phone.php
+++ bilemo_api_project/src/Entity/Phone.php
@@ -7,9 +7,11 @@ use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=PhoneRepository::class)
+ * @UniqueEntity("name")
  */
 class Phone
 {
@@ -22,17 +24,17 @@ class Phone
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(name="name", type="string", length=255)
      * @Groups({"list", "show"})
-     * @Assert\Length(min="2", max="255")
-     * @Assert\NotBlank
+     * @Assert\NotBlank(message="Le champ ne doit pas être vide")
+     * @Assert\Length(min="2", minMessage="Ce champ doit contenir un minimum de {{ limit }} caractères", max="255", maxMessage="Ce champ doit contenir un maximum de {{ limit }} caractères")
      */
     private $name;
 
     /**
      * @ORM\Column(type="string", length=255)
      * @Groups("show")
-     * @Assert\NotBlank
+     * @Assert\NotBlank(message="Le champ ne doit pas être vide")
      */
     private $description;
 
@@ -41,7 +43,8 @@ class Phone
      * @ORM\Column(type="string", length=255)
      * @Groups({"list", "show"})
      * @Assert\Range(min="0", max="1500")
-     * @Assert\NotBlank
+     * @Assert\NotBlank(message="Ce champ ne doit pas être vide")
+     * @Assert\Range(min="0", minMessage="La valeur minimum autorisée est {{ limit }}", max="1500", maxMessage="La valeur maximum autorisée est {{ limit }}")
      */
     private $price;
 
