c0d1793ccf30eff91d300337f6ee18556723e8bb
JgPhil <jamingph@gmail.com>
2020-09-02 09:35:30 +0200

    MPD created

--- bilemo_api_project/src/DataFixtures/PhonesFixtures.php
+++ bilemo_api_project/src/DataFixtures/PhonesFixtures.php
@@ -9,15 +9,15 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class PhonesFixtures extends Fixture
 {
-    private $names = ['iPhone', 'Samsung', 'Sony', 'Xiaomi', 'Motorola'];
+    private $names = ['iPhone', 'Samsung', 'Sony', 'Xiaomi', 'Motorola', 'Huawei', 'Lenovo', 'Alcatel', 'Asus'];
 
 
     public function load(ObjectManager $manager)
     {
         $faker = Faker\Factory::create('fr-FR');
-        for ($i = 1; $i <= 40; $i++) {
+        for ($i = 1; $i <= 120; $i++) {
             $phone = new Phone();
-            $phone->setName($this->names[rand(0, 4)] . ' ' . $faker->word());
+            $phone->setName($this->names[rand(0, 8)] . ' ' . $faker->word());
             $phone->setColor($faker->safeColorName);
             $phone->setPrice(rand(200, 1000));
             $phone->setDescription($faker->sentence(3));
